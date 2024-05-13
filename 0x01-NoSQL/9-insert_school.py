065d9d579c9d6b5f17455cf9dc9a3f5088fa3ad7
millyanne93 <nmillyanne20@gmail.com>
2024-05-13 10:52:01 -0700

    No sql

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" MongoDB Operations with Python using pymongo """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Inserts a new document in a collection based on kwargs """
+    new_school = kwargs
+    result = mongo_collection.insert_one(new_school)
+    return result.inserted_id
